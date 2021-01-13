6518edf5cb4d4c42791cde4fb2d68ac812724502
guibafica <gb.engcomp@hotmail.com>
2021-01-13 17:06:12 -0300

    FIX: Button color

--- src/components/InfoSection/index.js
+++ src/components/InfoSection/index.js
@@ -29,6 +29,9 @@ const InfoSection = ({
   description,
   buttonLabel,
   alt,
+  primary,
+  dark,
+  dark2,
 }) => {
   return (
     <>
@@ -44,7 +47,17 @@ const InfoSection = ({
                 <Subtitle darkText={darkText}>{description}</Subtitle>
 
                 <BtnWrap>
-                  <Button to="home">{buttonLabel}</Button>
+                  <Button 
+                    to="home"
+                    smooth={true}
+                    duration={500}
+                    spy={true}
+                    exact="true"
+                    offset={-80}
+                    primary={primary ? 1: 0}
+                    dark={dark ? 1 : 0}
+                    dark2={dark2 ? 1 : 0}
+                  >{buttonLabel}</Button>
                 </BtnWrap>
               </TextWrapper>
             </Column1>
