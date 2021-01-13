e016690b486e78be3f4e55a5e38f3c4f7fe29b6e
guibafica <gb.engcomp@hotmail.com>
2021-01-13 09:04:52 -0300

    CHORE: Estiliza HeroSection

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,15 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Video from '../../videos/video.mp4';
 
-import { HeroContainer, HeroBg, VideoBg } from './styles';
+import { 
+  HeroContainer, 
+  HeroBg, 
+  VideoBg, 
+  HeroContent,
+  HeroH1, 
+  HeroP,
+  HeroBtnWrapper,
+  ArrowForward,
+  ArrowRight,
+} from './styles';
 
 const HeroSection = () => {
+  const [hover, setHover] = useState(false);
+
+  const onHover = () => {
+    setHover(!hover);
+  };
+
   return (
     <HeroContainer>
       <HeroBg>
         <VideoBg autoPlay loop muted src={Video} type="video/mp4" />
       </HeroBg>
+
+      <HeroContent>
+        <HeroH1>Banco Virtual Facilitado</HeroH1>
+
+        <HeroP>
+          Cadastre-se para uma nova conta hoje e receba R$250 em crédito para o 
+          seu próximo pagamento.
+        </HeroP>
+
+        <HeroBtnWrapper>
+          <Button to="signup" onMouseEnter={onHover} onMouseLeave={onHover}>
+            Iniciar { hover ? <ArrowForward /> : <ArrowRight /> }
+          </Button>
+        </HeroBtnWrapper>
+      </HeroContent>
     </HeroContainer>
   );
 };
